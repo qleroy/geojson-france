ece5bf1f388596214b34fea644be47f2e7b70b42
Quentin Leroy <quentin.n.leroy@gmail.com>
2025-09-16 21:16:46 +0000

    fix 04_dump_multipolygons.sql

--- sql/04_dump_multipolygons.sql
+++ sql/04_dump_multipolygons.sql
@@ -1,10 +1,25 @@
-CREATE TABLE IF NOT EXISTS public.dz_dump AS
+DROP TABLE IF EXISTS public.dz_dump;
+CREATE TABLE public.dz_dump AS
+WITH parts AS (
+  SELECT
+    dz.id,
+    dz.name_dz,
+    (d).path  AS part_path,            -- ex: {1}, {2}, ...
+    (d).geom  AS geom_2154_part        -- géométrie POLYGON (ou MULTI si mono) en SRID d'origine
+  FROM public.dz
+  CROSS JOIN LATERAL ST_Dump(dz.geom_2154_simplified) AS d
+)
 SELECT
-  json_build_object(
-    'type','Feature',
-    'geometry', ST_AsGeoJSON(d.geom)::jsonb,
-    'properties', json_build_object()
+  id,
+  name_dz,
+  part_path[1]::int AS part_index,     -- index de la partie
+  geom_2154_part,                      -- la partie (POLYGON)
+  (
+    jsonb_build_object(
+      'type','Feature',
+      'geometry', ST_AsGeoJSON(ST_Transform(geom_2154_part, 4326))::jsonb,
+      'properties', jsonb_build_object('id', id, 'part', part_path[1])
+    )
   )::text AS feature
-FROM public.dz
-CROSS JOIN LATERAL ST_Dump(dz.geom) AS d;
+FROM parts;
 
